13db3242e1c8fa15103747060feb6170d6cc4f44
FAVQ <159665661+Felipe12389@users.noreply.github.com>
2025-07-19 21:59:41 +0100

    Added Weapon Ammo
    just added weapon ammo, still needs functions saying what weapons do when fired.

--- src/scripts/main_game_scene.gd
+++ src/scripts/main_game_scene.gd
@@ -1,16 +1,31 @@
 extends Control
 # a useless comment
+
+var mUses = 1
+var tUses = 1
+var fUses = 1
  
 func _on_menu_pressed() -> void:
 	get_tree().paused = true
 	$Control.show()
 
 func _on_mines_pressed() -> void:
-	pass # Replace with function body.
-
+	if mUses >= 1:
+		#add the weapon function
+		mUses -=1
+	else:
+		pass		
+	
 func _on_turrets_pressed() -> void:
-	pass # Replace with function body.
+	if tUses >= 1:
+		#add the weapon function
+		tUses -=1
+	else:
+		pass
 
 func _on_flame_thrower_pressed() -> void:
-	pass # Replace with function body.
-
+	if fUses >= 1:
+		#add the weapon function
+		fUses -=1
+	else:
+		pass
